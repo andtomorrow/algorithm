6c13c534c285deb196b3a8688d47b57efc092472
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2023-01-22 09:25:42 +0900

    [Bronze IV] Title: Congruent Numbers, Time: 128 ms, Memory: 41164 KB -BaekjoonHub

--- 백준/Bronze/15128. Congruent Numbers/Congruent Numbers.py
+++ 백준/Bronze/15128. Congruent Numbers/Congruent Numbers.py
@@ -8,7 +8,7 @@ leg1, leg2 = Fraction(p1,q1), Fraction(p2,q2)
 
 area = Fraction(leg1 * leg2, 2)
 
-if Fraction(area, ) == int(area):
+if Fraction(area, 1) == int(area):
 	result = 1
 else:
 	result = 0
