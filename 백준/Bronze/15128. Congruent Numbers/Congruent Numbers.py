9d32f5742fb72d4e6ba09e871b762f17e1b6f4be
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2023-01-22 09:20:53 +0900

    [Bronze IV] Title: Congruent Numbers, Time: 132 ms, Memory: 41208 KB -BaekjoonHub

---
+++ 백준/Bronze/15128. Congruent Numbers/Congruent Numbers.py
@@ -0,0 +1,16 @@
+from fractions import Fraction
+import cmath
+
+
+p1, q1, p2, q2 = map(int, input().split())
+
+leg1, leg2 = Fraction(p1,q1), Fraction(p2,q2)
+
+area = Fraction(leg1 * leg2, 2)
+
+if Fraction(area, ) == int(area):
+	result = 1
+else:
+	result = 0
+	
+print(result)
