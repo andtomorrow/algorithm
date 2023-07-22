6f183fed617811cc1a0c2d798c55af2bcb6cbdd2
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2023-07-22 17:30:07 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,18 +1,14 @@
 import sys
-from collections import Counter
 
 C = int(sys.stdin.readline())
-for case in range(C):
-    case_in = list(map(int, sys.stdin.readline().split()))
-    N, scores = case_in[0], case_in[1:]
-    
-    case_avg = sum(scores) / N
-    cnt = Counter(scores)
 
-    gt_avg_num = 0
-
-    for k, v in cnt.items():
-        if k > case_avg:
-            gt_avg_num += v
-    
-    print(f'{(gt_avg_num / N):.3%}')
+for t in range(C):
+    input_ln = list(map(int, sys.stdin.readline().split()))
+    N = input_ln[0]
+    avg = sum(input_ln[1:]) / N
+    cnt_gt_avg = 0
+    for num in input_ln[1:]:
+        if num > avg:
+            cnt_gt_avg += 1
+    case_result = cnt_gt_avg / N
+    print(f"{case_result:.3%}")
