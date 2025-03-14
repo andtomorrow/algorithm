34069bf734e3e52e8d88529f6c3d0fdc99b16cc0
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2025-03-14 20:30:16 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,10 +1,12 @@
-money = ['Q', 'D', 'N', 'P']
-money_cnt = [0, 0, 0, 0]
-unit = [25, 10, 5, 1]
+from sys import stdin
 
-T = int(input())
-for t in range(1, T+1):
-    amount = int(input())
-    for i in range(4):
-        money_cnt[i], amount = divmod(amount, unit[i])
-    print(*money_cnt)
+T = int(stdin.readline())
+
+for _ in range(T):
+    change_total = int(stdin.readline())
+    n_of_quarter, after_quarter = divmod(change_total, 25)
+    n_of_dime, after_dime = divmod(after_quarter, 10)
+    n_of_nickel, after_nickel = divmod(after_dime, 5)
+    n_of_penny = after_nickel
+
+    print(n_of_quarter, n_of_dime, n_of_nickel, n_of_penny, sep=" ")
