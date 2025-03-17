2b6d3c511cf6762306f116cf9e6fc7b135e54840
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2025-03-17 16:16:53 +0900

    [Bronze IV] Title: ISBN, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+FIRST_TEN_STR = "9780921418"
+
+isbn = FIRST_TEN_STR  # isbn: string
+
+for _ in range(3):
+    input_num = stdin.readline().strip()
+    isbn += input_num
+
+one_three_sum = 0
+
+for i in range(len(isbn)):
+    if i % 2 == 0:
+        one_three_sum += int(isbn[i]) * 1
+    else:
+        one_three_sum += int(isbn[i]) * 3
+
+print(f"The 1-3-sum is {one_three_sum}")
