8d836cb9d4a7a8425f2c0825977b06b01f9eb237
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2025-03-23 11:11:36 +0900

    [Bronze II] Title: 하얀 칸, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+board = [[False] * 8 for _ in range(8)]
+
+total_on_white = 0
+
+for i in range(8):  # 짝수행부터, 행 하나씩
+    input_line = stdin.readline().strip()
+
+    if i % 2 == 0:
+        for j in range(8):
+            if j % 2 == 0:  # 짝수열
+                if input_line[j] == "F":
+                    total_on_white += 1
+    else:
+        for j in range(8):
+            if j % 2 == 1:  # 홀수열
+                if input_line[j] == "F":
+                    total_on_white += 1
+
+print(total_on_white)
