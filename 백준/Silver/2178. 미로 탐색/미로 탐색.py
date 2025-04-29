0ee601dde79e5348300432d62e46fdbac39f781f
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2025-04-29 17:52:11 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34952 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+height, width = map(int, input().split())
+
+surface = [[0 for _ in range(width)] for _ in range(height)]
+
+for i in range(height):
+    new_inpt = input()
+    for j in range(width):
+        surface[i][j] = int(new_inpt[j])
+
+
+FOUR_DIRECTIONS = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+
+
+def bfs(y, x):
+    queue = deque()
+    queue.append((y, x))
+
+    while queue:
+        y, x = queue.popleft()
+
+        for i in range(4):
+            new_y = y + FOUR_DIRECTIONS[i][0]
+            new_x = x + FOUR_DIRECTIONS[i][1]
+
+            if 0 <= new_y < height and 0 <= new_x < width:
+                if surface[new_y][new_x] == 1:
+                    surface[new_y][new_x] = surface[y][x] + 1
+                    queue.append((new_y, new_x))
+
+
+bfs(0, 0)
+
+print(surface[height - 1][width - 1])
