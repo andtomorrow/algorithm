a0ce3adb3a5f5450fc83ac2df6b7f5013cbb0264
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2025-05-02 14:44:58 +0900

    [Bronze II] Title: 분해합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,16 +1,18 @@
+"""
+자연수 N에 대하여, 분해합: N과 N 각 자리수의 합
+예) 245의 분해합은 256 (=245+2+4+5)
+-생성자 (245는 256의 생성자. 생성자가 없는 경우도, 여러 개인 경우도 있음)
+구: 주어진 자연수 N의 가장 작은 생성자
+"""
+
 N = int(input())
 
-m = 0
-while True:
-    m += 1
-    m_str = str(m)
-    sum_each = sum([int(m_str[i]) for i in range(len(m_str))])
-    result_num = m + sum_each
-    
-    if result_num == N:
-        print(m)
+# N의 생성자 중 가장 작은 것을 구해야 함
+
+
+for num in range(max(1, N - 9 * len(str(N))), N + 1):
+    if num + sum(map(int, str(num))) == N:
+        print(num)
         break
-    
-    if m > N:
-        print(0)
-        break
+else:
+    print(0)
