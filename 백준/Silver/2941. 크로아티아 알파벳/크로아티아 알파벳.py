fbf09c5df2f28bbfad8b2e5208fbba11d94b0125
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2025-05-02 14:55:46 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,32 +1,14 @@
-s = input()
-out_ltr_cnt = 0
-three_ltr = ['dz=']
-two_ltr = [
-    'c=',
-    'c-',
-    'd-',
-    'lj',
-    'nj',
-    's=',
-    'z='
-]
-    
-while len(s) > 0:
-    if len(s) >= 3:
-        if s[0:3] in three_ltr:
-            s = s.replace(s[0:3], '', 1)
-            out_ltr_cnt += 1
-            continue
+CROATIAN = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-    if len(s) >= 2:
-        if s[0:2] in two_ltr:
-            s = s.replace(s[0:2], '', 1)
-            out_ltr_cnt += 1
-            continue
-            
-    if len(s) >= 1:
-        s = s.replace(s[0], '', 1)
-        out_ltr_cnt += 1
-        continue
+inpt_str = input()
+inpt_str_copied = inpt_str
 
-print(out_ltr_cnt)
+"""
+CROATIAN의 요소가 inpt_str에 있는지 확인 후, 해당 내용을 상관없는 임의의 한 글자로 처리
+"""
+
+for croatian_char in CROATIAN:
+    if croatian_char in inpt_str:
+        inpt_str_copied = inpt_str_copied.replace(croatian_char, "*")
+
+print(len(inpt_str_copied))
