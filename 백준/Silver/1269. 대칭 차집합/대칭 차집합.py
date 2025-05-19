4b4c979bb538e42b73ee90f4f06f6037f138d555
andtomorrow <68937273+andtomorrow@users.noreply.github.com>
2025-05-19 16:36:20 +0900

    [Silver IV] Title: 대칭 차집합, Time: 224 ms, Memory: 84588 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -1,6 +1,14 @@
-a, b = map(int, input().split())
+from sys import stdin
 
-A = set(map(int, input().split()))
-B = set(map(int, input().split()))
+"""
+공집합이 아닌 두 집합 A, B
+출력: 대칭 차집합의 원소의 개수
+대칭차집합 = (A-B)와 (B-A)의 합집합
+"""
 
-print(len(A | B) - len(A & B))
+num_A, num_B = map(int, input().split())
+
+A = set(map(int, stdin.readline().split()))
+B = set(map(int, stdin.readline().split()))
+
+print(len(A-B) + len(B-A))
